a8ad53a9e4d645b477f366e5b30fd78588cb1c78
Athreya B N <athreyabn03@gmail.com>
2024-06-21 23:46:04 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -13,5 +13,5 @@ export const assets ={
     upload_area,
     parcel_icon
 }
-
-export const url = 'http://localhost:4000'
+export const currency = '₹'
+export const url = 'https://food-del-backend-16c7.onrender.com'
